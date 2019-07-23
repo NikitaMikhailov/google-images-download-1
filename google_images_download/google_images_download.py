989c8472b7efd6e25801de56041b35c32ee0d538
Mikhailov Nikita <44605908+NikitaMikhailov@users.noreply.github.com>
2019-07-23 22:10:14 +0300

    Update google_images_download.py

--- google_images_download/google_images_download.py
+++ google_images_download/google_images_download.py
@@ -511,9 +511,10 @@ class googleimagesdownload:
     def download_image_thumbnail(self,image_url,main_directory,dir_name,return_image_name,print_urls,socket_timeout,print_size,no_download,save_source,img_src,ignore_urls):
         if print_urls or no_download:
             print("Image URL: " + image_url)
-            file_url=open('root/bot_herobot_ls/file_url.txt','w')
+            file_url=open('~/bot_herobot_ls/file_url.txt','w')
             file_url.write(image_url)
             file_url.close()
+            print('update ok')
         if no_download:
             return "success","Printed url without downloading"
         try:
@@ -582,9 +583,10 @@ class googleimagesdownload:
         if not silent_mode:
             if print_urls or no_download:
                 print("Image URL: " + image_url)
-                file_url=open('root/bot_herobot_ls//file_url.txt','w')
+                file_url=open('~/bot_herobot_ls//file_url.txt','w')
                 file_url.write(image_url)
                 file_url.close()
+                print('update ok')
         if ignore_urls:
             if any(url in image_url for url in ignore_urls.split(',')):
                 return "fail", "Image ignored due to 'ignore url' parameter", None, image_url
